445bf3fe97edce2866fcae67b108110324572776
Peter Kim <peter.minho.kim@gmail.com>
2020-08-19 15:08:47 -0400

    Add useEffect dependencies and cleanup (componentDidUnmount)

--- src/index.js
+++ src/index.js
@@ -37,11 +37,7 @@ const NoteApp = () => {
     <div>
       <h1>Notes</h1>
       {notes.map((note) => (
-        <div key={note.title}>
-          <h3>{note.title}</h3>
-          <p>{note.body}</p>
-          <button onClick={() => removeNote(note.title)}>x</button>
-        </div>
+        <Note key={note.title} note={note} removeNote={removeNote} />
       ))}
       <p>Add note</p>
       <form onSubmit={addNote}>
@@ -56,28 +52,20 @@ const NoteApp = () => {
   );
 };
 
-const App = (props) => {
-  const [count, setCount] = useState(props.count);
-  const [text, setText] = useState('');
-
+const Note = ({ note, removeNote }) => {
   useEffect(() => {
-    console.log('this should only run once');
+    console.log('use effect from Note');
+
+    return () => {
+      console.log('cleaning up effect');
+    };
   }, []);
-
-  useEffect(() => {
-    console.log('useEffect ran');
-    document.title = count;
-  }, [count]);
 
   return (
     <div>
-      <p>
-        the current {text || 'count'} is {count}
-      </p>
-      <button onClick={() => setCount(count + 1)}>+1</button>
-      <button onClick={() => setCount(count - 1)}>-1</button>
-      <button onClick={() => setCount(0)}>Reset</button>
-      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <h3>{note.title}</h3>
+      <p>{note.body}</p>
+      <button onClick={() => removeNote(note.title)}>x</button>
     </div>
   );
 };
